ea0bece4218fc3a657166896c96776e3ef31d2a5
Adeesha Ekanayake <adeeshaekanayake@gmail.com>
2016-10-29 23:13:34 -0400

    enabling multiprocessing for speed

--- control_experiment.py
+++ control_experiment.py
@@ -1,17 +1,31 @@
 import gc
+import sys
 
+from multiprocessing import Process, Queue, JoinableQueue
 from xlwt import Workbook
 
 import data_saver
 from control_simulation import ControlSimulation
 
-NUMBER_OF_SIMULATIONS = 100
+NUMBER_OF_SIMULATIONS = 300
+ITERATION_SENTINEL = 'STOP'
+NUMBER_OF_PROCESSES = 4
 
 def main():
     # disable gc for all experiment
     gc.disable()
-    control_experiment = ControlExperiment(300, NUMBER_OF_SIMULATIONS)
+    control_experiment = ControlExperiment(100, NUMBER_OF_SIMULATIONS)
     control_experiment.run()
+
+def worker(in_q, out_q):
+    while True:
+            simulation = in_q.get()
+            try:
+                simulation.run_simulation(False, False)
+                out_q.put(simulation)
+                print ('End of simulation #' + str(simulation.simulation_index + 1 ) + ' turns')
+            finally:
+                in_q.task_done()
 
 class ControlExperiment:
     recognition = False
@@ -20,6 +34,8 @@ class ControlExperiment:
     def __init__(self, number_of_generations=1, number_of_simulations=1):
         self.number_of_simulations = number_of_simulations
         self.number_of_generations = number_of_generations
+        self.to_do_queue = JoinableQueue()
+        self.done_queue = Queue()
 
     def run(self):
         total_population_record_list = []
@@ -84,7 +100,11 @@ class ControlExperiment:
                              total_acrossOMUwithinbandmean_list,
                              total_acrossOMUwithinbandsd_list)
 
+        print "saving data now"
+
         output_book.save(self.output_xl_name)
+
+        print "data saved"
 
     def run_loop(self, total_population_record_list,
                  total_age_record_list,
@@ -102,13 +122,33 @@ class ControlExperiment:
                  total_totalmean_list, total_totalsd_list,
                  total_acrossOMUwithinbandmean_list,
                  total_acrossOMUwithinbandsd_list):
+
         for i in range(self.number_of_simulations):
                 simulation = ControlSimulation()
                 simulation.set_number_of_generations(self.number_of_generations)
                 simulation.simulation_index = i
                 simulation.total_simulations = self.number_of_simulations
-                simulation.run_simulation(False, False)
+                self.to_do_queue.put(simulation)
+        
+        processes = []
 
+        for i in range(NUMBER_OF_PROCESSES):
+                p = Process(target=worker, args=(self.to_do_queue, self.done_queue))
+                p.daemon = True
+                p.start()
+                processes.append(p)
+
+        print "started all processes"
+        
+        self.to_do_queue.join()
+        
+        print "simulations complete - saving data"
+
+        self.done_queue.put(ITERATION_SENTINEL)
+
+        print "simulations complete - draining done_sims"
+
+        for simulation in iter(self.done_queue.get, ITERATION_SENTINEL):
                 total_population_record_list.append(
                         simulation.last_gen_population)
                 total_age_record_list.append(
@@ -139,9 +179,6 @@ class ControlExperiment:
                 total_acrossOMUwithinbandmean_list.append(simulation.acrossOMUwithinbandmean)
                 total_acrossOMUwithinbandsd_list.append(simulation.acrossOMUwithinbandsd)
 
-                print ('End of simulation #' + str(i + 1) + " @ " + str(self.number_of_generations) + " turns")
-                del (simulation)
-                gc.collect()
 
     def get_output_book(self,
                         book,
